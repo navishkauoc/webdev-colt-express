162f3845d536f08d7f707d7f324a16b488f84756
Navishka Kularathna <navishka.randika@gmail.com>
2019-12-09 14:12:15 +0530

    Route added

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var express = require("express");
 var app = express();
+
+app.get("/",function(req,res){
+    res.send("Hi there!");
+});
+
+app.listen(3000, function(){
+    console.log("Serving dog demo on port 3000");
+});
