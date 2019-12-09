79f106f08406610723da4c38cd0ee47f97659ec4
Navishka Kularathna <navishka.randika@gmail.com>
2019-12-09 15:46:45 +0530

    Second route added

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/",function(req,res){
     res.send("Hi there!");
 });
 
+app.get("/bye", function(req,res){
+    res.send("Goodbye!!");
+});
+
 app.listen(3000, function(){
     console.log("Serving dog demo on port 3000");
 });
