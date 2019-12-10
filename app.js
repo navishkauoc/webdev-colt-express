da7855dc5464b4eac099848701f1e16b520af23d
Navishka Kularathna <navishka.randika@gmail.com>
2019-12-10 14:41:12 +0530

    star route added

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get("/dog", function(req,res){
     res.send("MEOW!");
 });
 
+app.get("*", function(req,res){
+    res.send("You are a star!");
+});
+
+app.get("/r/:subredditName", function(req,res){
+    res.send("Welcome to a Subreddit!");
+});
+
 app.listen(3000, function(){
     console.log("Serving dog demo on port 3000");
 });
